bc0145e5b3b823da2b217cbc391185ada4d8316f
Dominik Kloepfer <dominikkloepfer@dhcp-90.wolf.ox.ac.uk>
2020-10-14 23:32:30 +0100

    cleaned up some visualisation code

--- visualisations.py
+++ visualisations.py
@@ -3,29 +3,58 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_true_data(x, y, x_label, y_label, y_noise=0):
+def plot_training_data(x, y, y_noise=0, x_label=None, y_label=None):
     '''
         Plots the data given by x and y using '+' with errorbars and adds labels and a legend.
+        This function is to be used for the training data from which the model predicts values.
 
         Args:
             x: n x 1 array of x-values
             y: n x 1 array of y-values
-            x_label: string; gives the label for the x-axis
-            y_label: string; gives the label for the y-axis
+            y_noise: n x 1 array or float (if equal noise for all values) of the standard deviations of the y-values; default is no noise 
+            x_label: optional string; gives the label for the x-axis
+            y_label: optional string; gives the label for the y-axis
+
+        Returns:
+            None
+    '''
+
+    plt.errorbar(x, y, yerr=y_noise, fmt='+', label='training data', color='black', zorder=0)
+
+    plt.legend()
+
+    if x_label:
+        plt.xlabel(x_label)
+    if y_label:
+        plt.ylabel(y_label)
+
+def plot_ground_truth(x, y, y_noise=0, x_label=None, y_label=None):
+    '''
+        Plots the data given by x and y using '+' with errorbars and adds labels and a legend.
+        This function is to be used for the ground truth data for which the model predicts values.
+
+        Args:
+            x: n x 1 array of x-values
+            y: n x 1 array of y-values
+            x_label: optional string; gives the label for the x-axis
+            y_label: optional string; gives the label for the y-axis
             y_noise: n x 1 array or float (if equal noise for all values) of the standard deviations of the y-values; default is no noise 
 
         Returns:
             None
     '''
 
-    plt.errorbar(x, y, yerr=y_noise, fmt='+', label='ground truths', color='black', zorder=0)
+    plt.errorbar(x, y, yerr=y_noise, fmt='+', label='ground truths', color='blue', zorder=0)
 
     plt.legend()
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
+
+    if x_label:
+        plt.xlabel(x_label)
+    if y_label:
+        plt.ylabel(y_label)
 
 
-def plot_mean(x, mean, x_label='', y_label=''):
+def plot_mean(x, mean, x_label=None, y_label=None):
     '''
         Plots the mean prediction of a Gaussian Process by interpolating the values in x and y.
 
@@ -43,12 +72,12 @@ def plot_mean(x, mean, x_label='', y_label=''):
 
     plt.legend()
 
-    if len(x_label) > 0:
+    if x_label:
         plt.xlabel(x_label)
-    if len(y_label) > 0:
+    if y_label:
         plt.ylabel(y_label)
 
-def plot_uncertainty(x, mean, cov_matrix, n_stdevs=1, x_label='', y_label=''):
+def plot_uncertainty(x, mean, cov_matrix, n_stdevs=1, x_label=None, y_label=None):
     '''
         Plots the uncertainty in the predicted mean by filling in the area a number of standard deviations above and below the mean.
 
@@ -77,3 +106,8 @@ def plot_uncertainty(x, mean, cov_matrix, n_stdevs=1, x_label='', y_label=''):
         plt.fill_between(np.squeeze(x), upper_limit, lower_limit, label='%d stdevs' %n, zorder=-10*n, color='red', alpha=1/(n+2), edgecolor='none')
 
     plt.legend()
+
+    if x_label:
+        plt.xlabel(x_label)
+    if y_label:
+        plt.ylabel(y_label)
